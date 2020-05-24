12d7135685a8530b7aad4c5be8337660b3512952
池臣 <1463301921@qq.com>
2020-05-24 16:22:36 +0800

    初始化

---
+++ spring-cloud-parent/cloud-common/src/main/java/com/cc/common/mybatisplus/CodeGenerator.java
@@ -0,0 +1,166 @@
+package com.cc.common.mybatisplus;
+
+import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.Scanner;
+
+/**
+ * @创建人 池臣
+ * @创建时间 2020/5/24
+ * @描述
+ */
+public class CodeGenerator {
+
+    /**
+     * <p>
+     * 读取控制台内容
+     * </p>
+     */
+    public static String scanner(String tip) {
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder help = new StringBuilder();
+        help.append("请输入" + tip + "：");
+        System.out.println(help.toString());
+        if (scanner.hasNext()) {
+            String ipt = scanner.next();
+            if (StringUtils.isNotEmpty(ipt)) {
+                return ipt;
+            }
+        }
+        throw new MybatisPlusException("请输入正确的" + tip + "！");
+    }
+
+    /**
+     * 数据库连接
+     */
+    private static String url="jdbc:mysql://58.49.23.178:9006/gczt_1.0?autoReconnect=true&useUnicode=true&characterEncoding=UTF-8&useSSL=false";
+    /**
+     * 数据库驱动
+     */
+    private static String driverName="com.mysql.jdbc.Driver";
+    /**
+     * 数据库连接用户名
+     */
+    private static String userName="root";
+    /**
+     * 数据库连接密码
+     */
+    private static String password="tbl_db_543";
+    /**
+     * 作者
+     */
+    private static String author = "池臣";
+    /**
+     * 父包名
+     */
+    private static String packageParent = "com.tbl.gczt";
+    /**
+     * 生成路径,到java目录那一级
+     */
+    private static String path = "D:\\IdeaProjects\\gczt\\gczt-server\\src\\main\\java";
+
+    public static void main(String[] args) {
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        //文件生成位置
+        gc.setOutputDir(path);
+        gc.setAuthor(author);
+        gc.setOpen(false);
+        // gc.setSwagger2(true); 实体属性 Swagger2 注解
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl(url);
+        //dsc.setSchemaName("public");
+        dsc.setDriverName(driverName);
+        dsc.setUsername(userName);
+        dsc.setPassword(password);
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        pc.setModuleName(scanner("模块名"));
+        pc.setParent(packageParent);
+        mpg.setPackageInfo(pc);
+
+        // 自定义配置
+        //InjectionConfig cfg = new InjectionConfig() {
+        //    @Override
+        //    public void initMap() {
+        //        to do nothing
+        //    }
+        //};
+
+        //生成xml
+        // 如果模板引擎是 freemarker
+        //String templatePath = "/templates/mapper.xml.ftl";
+        // 如果模板引擎是 velocity
+        // String templatePath = "/templates/mapper.xml.vm";
+
+        // 自定义输出配置
+        //List<FileOutConfig> focList = new ArrayList<>();
+        // 自定义配置会被优先输出
+        //focList.add(new FileOutConfig(templatePath) {
+        //    @Override
+        //    public String outputFile(TableInfo tableInfo) {
+        //        // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+        //        return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
+        //                + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+        //    }
+        //});
+        /*
+        cfg.setFileCreate(new IFileCreate() {
+            @Override
+            public boolean isCreate(ConfigBuilder configBuilder, FileType fileType, String filePath) {
+                // 判断自定义文件夹是否需要创建
+                checkDir("调用默认方法创建的目录");
+                return false;
+            }
+        });
+        */
+        //cfg.setFileOutConfigList(focList);
+        //mpg.setCfg(cfg);
+
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+
+        // 配置自定义输出模板
+        //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
+        // templateConfig.setEntity("templates/entity2.java");
+        // templateConfig.setService();
+        // templateConfig.setController();
+        //templateConfig.setXml(null);
+        //以上任意一个为null那么就不会生成该模块
+        mpg.setTemplate(templateConfig);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        //生成实体类注解
+        strategy.setEntityTableFieldAnnotationEnable(true);
+        //Lombok
+        strategy.setEntityLombokModel(true);
+        //strategy.setSuperEntityClass("com.baomidou.ant.common.BaseEntity");
+        strategy.setRestControllerStyle(true);
+        // 公共父类
+        //strategy.setSuperControllerClass("com.baomidou.ant.common.BaseController");
+        // 写于父类中的公共字段
+        //strategy.setSuperEntityColumns("id");
+        strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
+        strategy.setControllerMappingHyphenStyle(true);
+        strategy.setTablePrefix(pc.getModuleName() + "_");
+        mpg.setStrategy(strategy);
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
+
+}
---
+++ spring-cloud-parent/cloud-provider-payment8001/src/main/java/com/cc/SpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.cc;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @创建人 池臣
+ * @创建时间 2020/5/24
+ * @描述
+ */
+@org.springframework.boot.autoconfigure.SpringBootApplication
+public class SpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplication.class, args);
+    }
+}
