d1da5150993be59bac60f2e948ad24e67d9fb7c0
wakeUp-cc <313821660@qq.com>
2020-06-15 10:48:29 +0800

    配置中心

--- spring-cloud-parent/cloud-common-mybatis/src/main/java/com/cc/mybatisplus/config/DataSourceConfig.java
+++ spring-cloud-parent/cloud-common-mybatis/src/main/java/com/cc/mybatisplus/config/DataSourceConfig.java
@@ -32,36 +32,4 @@ public class DataSourceConfig {
         druidDataSource.setDriverClassName(driverClassName);
         return druidDataSource;
     }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getDriverClassName() {
-        return driverClassName;
-    }
-
-    public void setDriverClassName(String driverClassName) {
-        this.driverClassName = driverClassName;
-    }
 }
