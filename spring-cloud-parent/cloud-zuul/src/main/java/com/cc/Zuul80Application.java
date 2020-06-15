e2c83b71265a8e1dd0cf6944f040713eaa73ec80
wakeUp-cc <313821660@qq.com>
2020-06-15 19:00:15 +0800

    动态路由

--- spring-cloud-parent/cloud-zuul/src/main/java/com/cc/Zuul80Application.java
+++ spring-cloud-parent/cloud-zuul/src/main/java/com/cc/Zuul80Application.java
@@ -2,12 +2,14 @@ package com.cc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 /**
  * @author 池臣
  * @data 2020/6/12
  */
+@RefreshScope
 @EnableZuulProxy
 @SpringBootApplication
 public class Zuul80Application {
