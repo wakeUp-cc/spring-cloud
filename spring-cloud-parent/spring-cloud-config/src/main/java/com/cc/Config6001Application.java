62a6639ba1ec29b02e22a41c371a69f165c09122
wakeUp-cc <313821660@qq.com>
2020-06-15 15:54:26 +0800

    配置中心结合eureka

--- spring-cloud-parent/spring-cloud-config/src/main/java/com/cc/Config6001Application.java
+++ spring-cloud-parent/spring-cloud-config/src/main/java/com/cc/Config6001Application.java
@@ -3,12 +3,14 @@ package com.cc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author 池臣
  * @data 2020/6/15
  */
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class Config6001Application {
     public static void main(String[] args) {
