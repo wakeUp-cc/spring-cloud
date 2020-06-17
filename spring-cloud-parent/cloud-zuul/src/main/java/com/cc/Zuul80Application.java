9a147d45d7582230fad4815d7d14df2517fa4ccd
wakeUp-cc <313821660@qq.com>
2020-06-17 20:00:22 +0800

    注册中心改为consul

--- spring-cloud-parent/cloud-zuul/src/main/java/com/cc/Zuul80Application.java
+++ spring-cloud-parent/cloud-zuul/src/main/java/com/cc/Zuul80Application.java
@@ -2,6 +2,7 @@ package com.cc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
@@ -11,6 +12,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
  */
 @RefreshScope
 @EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Zuul80Application {
     public static void main(String[] args) {
--- spring-cloud-parent/spring-cloud-config/src/main/java/com/cc/Config6001Application.java
+++ spring-cloud-parent/spring-cloud-config/src/main/java/com/cc/Config6001Application.java
@@ -2,16 +2,17 @@ package com.cc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author 池臣
  * @data 2020/6/15
  */
 @EnableConfigServer
-@EnableEurekaClient
-@SpringBootApplication
+//@EnableEurekaClient
+@EnableDiscoveryClient
+@SpringBootApplication(scanBasePackages = "com.cc")
 public class Config6001Application {
     public static void main(String[] args) {
         SpringApplication.run(Config6001Application.class, args);
