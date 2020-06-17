a797d4e017d194df1c05a7d330f8e2a2fc868365
wakeUp-cc <313821660@qq.com>
2020-06-17 20:07:21 +0800

    注册中心改为consul

--- spring-cloud-parent/cloud-provider-payment8002/src/main/java/com/cc/Payment8002Application.java
+++ spring-cloud-parent/cloud-provider-payment8002/src/main/java/com/cc/Payment8002Application.java
@@ -3,14 +3,15 @@ package com.cc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @创建人 池臣
  * @创建时间 2020/5/24
  * @描述
  */
-@EnableEurekaClient
+//@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = "com.cc")
 public class Payment8002Application {
     public static void main(String[] args) {
